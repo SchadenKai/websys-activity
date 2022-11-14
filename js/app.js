388bd23a03521def6c718c10f5cd19d715e4eff3
Kai Tecson <kairustecsonpersonal@gmail.com>
2022-11-14 14:12:30 +0800

    changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 $('#cat_img').click(function(e) {
     if ($('#cat_img').css("width") == "200px") {
-        console.log("tanginamo");
         $('#cat_img').css("width","10%");
     } else {
         $('#cat_img').css("width","200px")
